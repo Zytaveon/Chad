0f7dc884c6aad09d9eafb5eee6cb0365d5d5793d
dmcmann <dmcmann@linux-2.ece.iastate.edu>
2018-01-17 15:34:51 -0600

    Tried to fix it

--- dot.c
+++ dot.c
@@ -2,7 +2,7 @@ double dot(double a[], double b[], int length) {
 	int i;
 	double sum;   
 	for (i=0; i < length; i++) { 
-		sum = sum + a[i]*b[i];
+		sum = sum + (a[i]*b[i]);
 		}
 	return sum;
 	}
